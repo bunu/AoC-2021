bf1c3f3ef07aecb22ece6e81cb1a7c7c764bc803
James Brookhouse <james.brookhouse@gmail.com>
2021-12-08 14:33:22 +0000

    Day 8 solutions (improved)

--- day_08/part2.py
+++ day_08/part2.py
@@ -1,23 +1,7 @@
 from typing import List, Dict
 
 
-def count_letters(code):
-    counts = {}
-    for s in code:
-        counts[s] = 1
-    return counts
-
-
-def string_contains(code: str, check: List[str]) -> bool:
-    one_letters = count_letters(check)
-    code_letters = count_letters(code)
-    for letter in one_letters:
-        if not code_letters.get(letter, 0):
-            return False
-    return True
-
-
-def find_one(code_lookup: Dict[int, str], codes: List[str]):
+def find_one(code_lookup: Dict[int, set], codes: List[set]):
     for code in codes:
         if len(code) == 2:
             code_lookup[1] = code
@@ -25,23 +9,23 @@ def find_one(code_lookup: Dict[int, str], codes: List[str]):
             return
 
 
-def find_two(code_lookup: Dict[int, str], codes: List[str]):
+def find_two(code_lookup: Dict[int, set], codes: List[set]):
     for code in codes:
-        if len(code) == 5 and string_contains(code, list(set(code_lookup[6]) - set(code_lookup[9]))):
+        if len(code) == 5 and code_lookup[6].difference(code_lookup[9]).issubset(code):
             code_lookup[2] = code
             codes.remove(code)
             return
 
 
-def find_three(code_lookup: Dict[int, str], codes: List[str]):
+def find_three(code_lookup: Dict[int, set], codes: List[set]):
     for code in codes:
-        if len(code) == 5 and string_contains(code, list(code_lookup[1])):
+        if len(code) == 5 and code_lookup[1].issubset(code):
             code_lookup[3] = code
             codes.remove(code)
             return
 
 
-def find_four(code_lookup: Dict[int, str], codes: List[str]):
+def find_four(code_lookup: Dict[int, set], codes: List[set]):
     for code in codes:
         if len(code) == 4:
             code_lookup[4] = code
@@ -49,7 +33,7 @@ def find_four(code_lookup: Dict[int, str], codes: List[str]):
             return
 
 
-def find_five(code_lookup, codes):
+def find_five(code_lookup: Dict[int, set], codes: List[set]):
     for code in codes:
         if len(code) == 5:
             code_lookup[5] = code
@@ -57,7 +41,7 @@ def find_five(code_lookup, codes):
             return
 
 
-def find_six(code_lookup, codes):
+def find_six(code_lookup: Dict[int, set], codes: List[set]):
     for code in codes:
         if len(code) == 6:
             code_lookup[6] = code
@@ -65,7 +49,7 @@ def find_six(code_lookup, codes):
             return
 
 
-def find_seven(code_lookup: Dict[int, str], codes: List[str]):
+def find_seven(code_lookup: Dict[int, set], codes: List[set]):
     for code in codes:
         if len(code) == 3:
             code_lookup[7] = code
@@ -73,7 +57,7 @@ def find_seven(code_lookup: Dict[int, str], codes: List[str]):
             return
 
 
-def find_eight(code_lookup: Dict[int, str], codes: List[str]):
+def find_eight(code_lookup: Dict[int, set], codes: List[set]):
     for code in codes:
         if len(code) == 7:
             code_lookup[8] = code
@@ -81,9 +65,9 @@ def find_eight(code_lookup: Dict[int, str], codes: List[str]):
             return
 
 
-def find_nine(code_lookup: Dict[int, str], codes: List[str]):
+def find_nine(code_lookup: Dict[int, set], codes: List[set]):
     for code in codes:
-        if len(code) == 6 and string_contains(code, list(set.union(set(code_lookup[4]), set(code_lookup[7])))):
+        if len(code) == 6 and code_lookup[4].union(code_lookup[7]).issubset(code):
             code_lookup[9] = code
             codes.remove(code)
             return
@@ -91,7 +75,7 @@ def find_nine(code_lookup: Dict[int, str], codes: List[str]):
 
 def find_zero(code_lookup, codes):
     for code in codes:
-        if len(code) == 6 and string_contains(code, code_lookup[7]):
+        if len(code) == 6 and code_lookup[7].issubset(code):
             code_lookup[0] = code
             codes.remove(code)
             return
@@ -113,16 +97,20 @@ def main():
         9: find_nine,
     }
     for line in problem:
-        code_lookup = {i: "" for i in range(10)}
-        codes = ["".join(sorted(s)) for s in line[0].split()]
+        code_lookup = {}
+        codes = [frozenset(s) for s in line[0].split()]
+        digits = [frozenset(s) for s in line[1].split()]
+
+        # Construct the code mappings
         for i in [1, 4, 7, 8, 9, 0, 6, 3, 2, 5]:
             function = switch[i]
             function(code_lookup, codes)
-        digits = [s for s in line[1].split()]
+
+        # Decode the display
         number = ""
         inverted_lookup = {v: str(k) for k, v in code_lookup.items()}
         for digit in digits:
-            number += inverted_lookup["".join(sorted(digit))]
+            number += inverted_lookup[digit]
         count += int(number)
     print("Part2 Answer: %s" % count)
 
